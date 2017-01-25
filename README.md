73065c6969b20766a952d19bc98306ad0a615f0e
Alex Nichol <unixpickle@gmail.com>
2017-01-25 13:46:51 -0500

    basic description of language

---
+++ README.md
@@ -0,0 +1,3 @@
+# convmarkup
+
+This is a markup language for describing convolutional neural networks. See more details in the [Godoc](https://godoc.org/github.com/unixpickle/convmarkup).
---
+++ doc.go
@@ -0,0 +1,89 @@
+// Package convmarkup parses a markup language for
+// describing neural networks.
+//
+// Format overview
+//
+// Everything in the markup language is a block.
+// A block has a set of numerical attributes and,
+// potentially, a set of sub-blocks.
+// A markup file is inside an implicit "root block".
+//
+// Blocks with no children are declared on their own line
+// like so:
+//
+//     BlockName(attr1=val1, attr2=val2, attr3=val3)
+//
+// As a concrete example, you might declare a layer like
+// this:
+//
+//     Conv(w=3, h=3, n=64, sx=1, sy=1)
+//
+// If a block has no attributes, the parentheses can be
+// omitted, such as in:
+//
+//     BatchNorm
+//     MaxPool(w=2, h=2)
+//     ReLU
+//
+// When a block has children, you use curly braces and
+// typically indent the contents of the block:
+//
+//     Residual {
+//         Padding(l=1, r=1, t=1, b=1)
+//         Conv(w=3, h=3, n=64)
+//     }
+//
+// Every block takes a tensor as input and produces a
+// tensor as output.
+// Blocks must be aware of how they manipulate tensor
+// dimensions.
+// To tell the first block about its input dimensions,
+// every file must begin with an Input block:
+//
+//     Input(w=224, h=224, d=3)
+//
+// Block types
+//
+// There are a number of built-in block types for creating
+// convolutional neural networks.
+// This section describes each one and its attributes.
+//
+// The Input block should be the first block in every file
+// and determines the input tensor dimensions.
+// It has three attributes: w for width, h for height, and
+// d for depth.
+//
+// The Conv block defines a convolutional layer.
+// The w and h attributes control filter width and height.
+// The n attribute controls the number of filters.
+// Optional sx and sy attributes determine the x and y
+// strides.
+// Absent strides are assumed to be 1.
+//
+// The MaxPool block defines a max-pooling layer.
+// The w and h attributes set the pool width and height.
+//
+// The BatchNorm block is a batch normalization layer.
+//
+// The ReLU block is a ReLU activation layer.
+//
+// The Padding block performs tensor padding.
+// It has four attributes, t, b, r, l, for top, bottom,
+// right, and left padding respectively.
+//
+// The Residual block defines a residual grouping.
+// A Residual's sub-blocks are combined and used as the
+// residual mapping.
+// If the residual mapping needs to do a projection to
+// resize the input, an optional Projection block can be
+// used:
+//
+//     Residual {
+//         Projection {
+//             Conv(w=1, h=1, n=64)
+//         }
+//         Padding(l=1, r=1, t=1, b=1)
+//         Conv(w=3, h=3, n=64)
+//     }
+//
+package convmarkup
